2f7c9b69f6568ddd70c08297045434f087a355a4
Sandesh <64647847+sandesh721@users.noreply.github.com>
2024-10-29 14:31:17 +0530

    Add files via upload

---
+++ Tressure hunt/Python/car.py
@@ -0,0 +1,12 @@
+# You are traveling on the highway, where a speedometer is installed. The speedometer's sensor records the entry and exit times of a car. Given that the car is 3 meters long, how would you calculate the speed of the car as a programmer?
+
+#entry and exit time is in seconds, lengthOfCar is in meters
+#converting m/s to km/hr is 18/5;
+
+entryTime = 0
+exitTime = 0.127
+lengthOfCar = 3
+
+# Output should be in int
+
+#Convert the speed to character
