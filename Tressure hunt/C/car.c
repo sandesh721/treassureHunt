246043a15f71fa6713e070cbd51259d39b2a0a6d
Sandesh721 <sandeshnagaraja7@gmail.com>
2024-11-01 14:48:41 +0530

    new files added

---
+++ Tressure hunt/C/car.c
@@ -0,0 +1,19 @@
+// You are traveling on the highway, where a speedometer is installed. The speedometer's sensor records the entry and exit times of a car. Given that the car is 3 meters long, how would you calculate the speed of the car as a programmer?
+
+// entry and exit time is in seconds, lengthOfCar is in meters
+// converting m/s to km/hr is 18/5;
+
+#include <stdio.h>
+
+int main() {
+    float entryTime = 0.0;
+    float exitTime = 0.127;
+    int lengthOfCar = 3;
+
+    // Output should be in int
+    
+
+    // Convert the speed to character
+    return 0;
+}
+
---
+++ Tressure hunt/JAVA/startFinding.java
@@ -0,0 +1,8 @@
+// Writet the program to decrypt the given string to valid Key string to open the clue.txt
+// formula: eachCharacter-key
+public class startFinding {
+    public static void main(String[] args) {
+        int key = 3;
+        String encryptedString = "wuhdvvxuh";
+    }
+}
